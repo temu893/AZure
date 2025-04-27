588cb9ee5f6f00bf3cdc22901511c8d192e96321
temesgen <65918861+temu893@users.noreply.github.com>
2025-04-27 05:03:43 -0500

    Create vm.sh

---
+++ day-1-azure-vm-deployment/vm.sh
@@ -0,0 +1,1 @@
+az vm create --name VM-2 --resource-group TemDevOps --admin-username T**** --admin-password V****** --image MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition-core:latest
