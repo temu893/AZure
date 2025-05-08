bb242b0a9ec60ce2e85e097fc861c6bb5ed04847
temesgen <65918861+temu893@users.noreply.github.com>
2025-05-08 08:38:03 +0000

    prod env

---
+++ day-7-webapp-architecture/prod/main.tf
@@ -0,0 +1,75 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# NETWORK MODULE
+module "network" {
+  source              = "../modules/network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  vnet_cidr           = var.vnet_cidr
+  web_subnet_cidr     = var.web_subnet_cidr
+  app_subnet_cidr     = var.app_subnet_cidr
+  db_subnet_cidr      = var.db_subnet_cidr
+  admin_ip            = var.admin_ip
+}
+
+#PUBLIC IP FOR VM
+
+resource "azurerm_public_ip" "vm_ip" {
+  name                = "vm-web-publicIp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Basic"
+
+}
+
+#NETWORK INTERFACE FOR VM
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "vm-web-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "webipconfig"
+    subnet_id                     = module.network.web_subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_ip.id
+  }
+}
+
+# LINUX VM IN WEB SUBNET 
+
+resource "azurerm_linux_virtual_machine" "web_vm" {
+  name                            = "vm-web"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = var.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  network_interface_ids           = [azurerm_network_interface.vm_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "vm-web-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
---
+++ day-7-webapp-architecture/prod/variables.tf
@@ -0,0 +1,63 @@
+variable "resource_group_name" {
+  type = string
+}
+variable "location" {
+  type    = string
+  default = "centralus"
+
+}
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID to deploy into"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+variable "vnet_name" {
+  type        = string
+  description = "name of the virtual network"
+  default     = "MyVnet"
+
+}
+variable "vnet_address_space" {
+  type        = string
+  description = "CIDR range for the VNet"
+  default     = "10.0.0.0/16"
+}
+
+variable "nsg_name" {
+  type        = string
+  description = "Name of the network security group"
+  default     = "MyNSG"
+
+}
+variable "vm_name" {
+  type        = string
+  description = "Name of the virtual machine"
+  default     = "MyWindowVM"
+
+}
+variable "admin_username" {
+  type        = string
+  description = "admin user for the VM"
+
+}
+variable "admin_password" {
+  type        = string
+  description = "Admin password for the vm"
+  sensitive   = true
+
+}
+variable "admin_ip" {
+  type        = string
+  description = "public ip in CIDR format for RDO access"
+}
+variable "vnet_cidr" {
+  description = "CIDR range for VNet"
+  type        = string
+}
+variable "web_subnet_cidr" {}
+variable "app_subnet_cidr" {}
+variable "db_subnet_cidr" {}
